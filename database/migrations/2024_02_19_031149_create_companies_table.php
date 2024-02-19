aee4a95a9163b29aa7e2bfa451cc02d922a83208
jah09 <jasuhot1@gmail.com>
2024-02-19 11:27:29 +0800

    add table columns and table relationships

---
+++ database/migrations/2024_02_19_031149_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('logo_url');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal');
+            $table->integer('tel');
+            $table->string('email');
+            $table->string('website');
+            $table->dateTime('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
