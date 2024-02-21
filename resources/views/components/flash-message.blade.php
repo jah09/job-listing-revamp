29c2f22dade42f97f09f70cdae7266922ba9583e
jah09 <jasuhot1@gmail.com>
2024-02-21 11:23:49 +0800

    modify flash to right but not working as of now

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,6 @@
 @if (session()->has('message'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-        class="fixed bottom-0 bottom-0 left-1/2 transform -translate-x-1/2 bg-red-900 text-white px-48 py-3">
+        class="fixed right-0 right-0 right-1/2 transform -translate-x-1/2 bg-red-900 text-white px-0 py-0">
         <p>
             {{ session('message') }}
         </p>
