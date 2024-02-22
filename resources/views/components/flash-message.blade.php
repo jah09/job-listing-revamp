25c488763aa213c3988d3f2a8f474a2966d78571
jah09 <jasuhot1@gmail.com>
2024-02-22 17:05:32 +0800

    add the sweet alert codes

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,8 +1,20 @@
-@if (session()->has('message'))
-    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-        class="fixed right-0 right-0 right-1/2 transform -translate-x-1/2 bg-red-900 text-white px-0 py-0">
-        <p>
-            {{ session('message') }}
-        </p>
-    </div>
+@if (session()->has('success'))
+    <script>
+        Swal.fire({
+            icon: "success",
+            title: "{{ session('success') }}",
+            confirmButtonText: "OK",
+            timer: 2500
+        });
+    </script>
+@elseif(session()->has('error'))
+    <script>
+        Swal.fire({
+            icon: "error",
+            title: "{{ session('error') }}",
+            confirmButtonText: "OK",
+            confirmButtonColor: "#023047",
+            timer: 2500
+        });
+    </script>
 @endif
