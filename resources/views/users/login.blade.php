cd00d67c84956a90675d7657c36171bb350d4ce9
jah09 <jasuhot1@gmail.com>
2024-02-23 23:34:34 +0800

    change the login UI

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,52 +1,65 @@
-<x-layout >
+<x-layout>
     <x-card>
-        <div class="justify-center flex ">
-            <img src="{{ asset('images/logo.png') }}" alt="logo" class="w-24">
-
-        </div>
-        <div class=" ">
-            <form action="/users/authenticate" method="POST" class="mt-10 bg-gray-50 border border-gray-500 rounded  max-w-lg mx-auto p-10">
-                @csrf
-                <div class="flex items-center flex-col">
-                    <h1 class="font-bold text-4xl">Login</h1>
-                    <p class="text-lg">Login to your account</p>
-                </div>
-                <div class="mb-6 mt-10">
-                    <label for="email" class="inline-block text-lg mb-2">Email</label>
-                    <input value="{{ old('email') }}" type="email" class="border border-gray-200 rounded p-2 w-full"
-                        name="email" />
-                    @error('email')
-                        <p class="text-red-500 text-sm font-semibold mt-1">{{ $message }}</p>
-                    @enderror
+       
+        <section class="min-h-screen flex items-stretch text-white ">
+            <div class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center"
+                style="background-image: url(https://images.pexels.com/photos/590037/pexels-photo-590037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);">
+                <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
+                <div class="w-full px-24 z-10">
+                    <h1 class="text-5xl font-bold text-left tracking-wide">Get the latest job!</h1>
+                    <p class="text-3xl my-4 italic">Start your journey with us, now.</p>
                 </div>
 
-                <div class="mb-6">
-                    <label for="password" class="inline-block text-lg mb-2">
-                        Password
-                    </label>
-                    <input value="{{ old('password') }}" type="password"
-                        class="border border-gray-200 rounded p-2 w-full" name="password" />
-                    @error('password')
-                        <p class="text-red-500 text-sm font-semibold mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                {{-- Button codes --}}
-                <div class="mb-6 flex justify-center">
-                    <button type="submit"
-                        class="bg-gray-50 font-semibold   rounded py-2 px-4 border border-red-600 text-black hover:bg-red-600 hover:text-white">
-                        Sign In
-                        <i class="fa fa-sign-in ml-2" aria-hidden="true"></i>
-                    </button>
-                </div>
-                <div class="">
-                    <p class="font-semibold">
-                        Don't have an account?
-                        <a href="/register">
-                            <span class="text-red-500">Register</span></a>
-                    </p>
-                </div>
+            </div>
+            <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0"
+                style="background-color:  #023047;">
+          
+                <div class="w-full py-6 z-20">
+                    <div class="flex flex-col items-center">
+                        <img src="{{ asset('images/logo.png') }}" alt="logo" class="w-24">
+                        <h1 class="font-bold text-4xl my-6">
+                            Login
+                        </h1>
+                    </div>
+                   
 
-            </form>
-        </div>
+
+                    <form action="/users/authenticate" method="POST" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+                        @csrf
+                        <div class="pb-2 pt-4">
+                            <input type="email" name="email" id="email" placeholder="Email"
+                                class="block w-full p-4 text-lg rounded-sm bg-black" value="{{ old('email') }}">
+                            @error('email')
+                                <p class="text-red-500 text-sm font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="pb-2 pt-4">
+                            <input class="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password"
+                                id="password" placeholder="Password" value="{{ old('password') }}">
+                            @error('password')
+                                <p class="text-red-500 text-sm font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="text-right text-gray-400 hover:underline hover:text-gray-100">
+                            <a href="#">Forgot your password?</a>
+                        </div>
+                        <div class=" pb-2 pt-6">
+                            <button type="submit"
+                                class="uppercase block w-full p-4 text-lg rounded-md border border-[#229fe7] hover:bg-[#229fe7] hover:text-black focus:outline-none font-semibold">sign
+                                in <i class="fa fa-sign-in ml-2" aria-hidden="true"></i></button>
+                        </div>
+                        <div class="mt-20">
+                            <p class="font-semibold">
+                                Don't have an account?
+                                <a href="/register">
+                                    <span class="text-[#229fe7]">Register now.</span></a>
+                            </p>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </section>
     </x-card>
 </x-layout>
