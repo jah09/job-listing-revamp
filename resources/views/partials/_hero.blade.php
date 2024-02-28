8b4d7e75040809d488e4a802054ab67eee3f8c02
jah09 <jasuhot1@gmail.com>
2024-02-28 10:04:19 +0800

    reading and exploring the codes and putting some comments

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,15 +1,16 @@
-<section class="select-none relative h-screen bg-[#023047] flex flex-col justify-center align-center text-center">
+<section class="select-none  relative h-screen bg-[#023047] justify-center flex flex-col align-center text-center">
+    {{-- ang whole na section is naka relative so dapat ang mga child div is naka absolute pra dili masaag sa laen nga div/section pud, para ang kana na mga div,diha ra mag libut2--}}
     {{-- @include('partials._header') --}}
     <!-- include the header component partial !-->
-    <div class="absolute top-0 right-0 flex justify-center items-center w-1/2 h-full ">
+    <div class="absolute top-0 right-0 flex justify-center items-center w-1/2 h-full  ">
         <img class="pb-24" src="images/footer-object.gif" alt="">
     </div>
     <div class="absolute top-0 bottom-[20px] flex w-full h-full opacity-20 " style="">
         <img class="justify-center items-center" src="images/bg_p-information.png" alt="">
     </div>
 
-    
-    <div class="max-w-7xl container mx-auto hidden lg:block z-10 text-start relative ">
+
+    <div class="max-w-7xl container mx-auto hidden lg:block z-10 text-start relative  ">
         <span class="absolute text-xs font-semibold text-white pt-4 ps-4">YOURSURNAME'S</span>
         <h1 class="text-[84px] font-bold uppercase text-[#ffa903]">
             JOB<span class="text-[#229fe7] font-semibold">LISTING</span>
