ae87031cc6dec222928c70820bc7812e40ce8312
jah09 <jasuhot1@gmail.com>
2024-02-28 15:37:37 +0800

    include the resume in job listing applicant page

--- resources/views/users/dashboard/jobapplicants.blade.php
+++ resources/views/users/dashboard/jobapplicants.blade.php
@@ -2,7 +2,7 @@
     <div class="px-12 mt-8 overflow-hidden rounded-lg border border-gray-200">
         @foreach ($jobListings as $item)
             <div class="flex justify-between items-center mb-10">
-                <h1 class="text-[44px] font-sans font-bold">Applicants for - {{ $item-> job_title }}</h1>
+                <h1 class="text-[44px] font-sans font-bold">Applicants for - {{ $item->job_title }}</h1>
                 {{-- <div class="">
                 <a href="{{route('dashboard.createCompany')}}">
                 <button class="bg-hipe-dark-blue text-md py-2 px-6 text-white rounded-lg shadow-sm outline-none">Create company</button>
@@ -13,11 +13,12 @@
         <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
             <thead class="bg-gray-100">
                 <tr>
-                    <th scope="col" class="px-6 py-4 font-medium text-gray-900 w-40">Application ID</th>
-                    <th scope="col" class=" py-4 font-medium text-gray-900">First Name</th>
+                    <th scope="col" class="px-6 py-4 font-medium text-gray-900 w-16"> ID</th>
+                    <th scope="col" class=" py-4 font-medium text-gray-900 ">First Name</th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Last Name</th>
-                    <th scope="col" class="py-4 font-medium text-gray-900">Contact Number</th>
-                    <th scope="col" class="py-4 font-medium text-gray-900 ">Email Address</th>
+                    <th scope="col" class="py-4 font-medium text-gray-900"> Number</th>
+                    <th scope="col" class="py-4 font-medium text-gray-900 ">Email </th>
+                    <th scope="col" class="py-4 font-medium text-gray-900 ">Resume </th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">Status</th>
                 </tr>
             </thead>
@@ -46,8 +47,15 @@
                             <td class="py-4">
                                 <p class=" text-gray-700">{{ $item->user->email }}</p>
                             </td>
+                            <td class="py-4">
+                                <a href="{{ asset('storage/' . $item->user_resume->resume_url) }}" target="_blank">
 
-                            <td class="px-4">
+
+                                    <p class=" text-gray-700 hover:font-medium">{{ $item->user_resume->name }}</p>
+                                </a>
+                            </td>
+
+                            <td class="px-4 ">
                                 <form id="form_{{ $item->id }}" method="POST"
                                     action="applicant/{applicant_id}/update-status" enctype="multipart/form-data">
                                     @csrf
@@ -80,7 +88,6 @@
                                 </form>
                             </td>
                         </tr>
-                       
                     @endforeach
                 @else
                     <tr class="hover:bg-gray-50">
