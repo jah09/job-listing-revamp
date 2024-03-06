c375467c0b82e28f2169978e955b2e6e6cefb8a6
jah09 <jasuhot1@gmail.com>
2024-03-06 17:02:14 +0800

    made a UI for search button, model scope function and its functionality

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -7,8 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobListing extends Model
 {
-    protected $fillable = ['company_id', 'user_id', 'job_category_id', 'min_monthly_salary', 'max_monthly_salary', 'description', 'employment_type','job_title'];
+    protected $fillable = ['company_id', 'user_id', 'job_category_id', 'min_monthly_salary', 'max_monthly_salary', 'description', 'employment_type', 'job_title'];
     use HasFactory;
+    public function scopeFilter($query, array $filters)
+    {
+    
+        if ($filters['search'] ?? false) {
+            $searchTerm = request('search');
+            $query->where(function ($query) use ($searchTerm) {
+                $query->where('job_title', 'like', '%' . $searchTerm . '%')
+                    ->orWhere('description', 'like', '%' . $searchTerm . '%');
+            });
+        }
+    }
 
     public function user()
     {
@@ -20,9 +31,9 @@ class JobListing extends Model
         return $this->belongsTo(Company::class);
     }
 
-    public function job_category(){
+    public function job_category()
+    {
         return $this->belongsTo(JobCategory::class);
-
     }
 
     // public function job_listing()
@@ -36,7 +47,8 @@ class JobListing extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function job_listings(){
+    public function job_listings()
+    {
         return $this->hasMany(JobApplication::class);
     }
 }
